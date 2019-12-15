929be6ce829a0cfd673134824d72b10b7ec6e020
AntePost <is1213791@gmail.com>
2019-12-15 19:38:08 +0300

    Add partial homework for lessons 7 & 8

--- assets/CalendarAsset.php
+++ assets/CalendarAsset.php
@@ -15,17 +15,15 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class AppAsset extends AssetBundle
+class CalendarAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        './css/calendar.css',
     ];
     public $js = [
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
     ];
 }
